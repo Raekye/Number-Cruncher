184aab6331fe2fa1402aa5f032a4cfd18078e053
Raekye <adrian@creatifcubed.com>
2014-02-21 13:57:04 -0500

    empty project

---
+++ Number Cruncher/Number CruncherTests/Number_CruncherTests.m
@@ -0,0 +1,34 @@
+//
+//  Number_CruncherTests.m
+//  Number CruncherTests
+//
+//  Created by Adrian on 2/21/2014.
+//  Copyright (c) 2014 Creatif Cubed. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Number_CruncherTests : XCTestCase
+
+@end
+
+@implementation Number_CruncherTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
