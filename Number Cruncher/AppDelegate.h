e22c9be49179730b713a01abee226244192e8b20
Raekye <adrian@creatifcubed.com>
2014-02-21 14:00:56 -0500

    Initial Commit

---
+++ Number Cruncher/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Number Cruncher
+//
+//  Created by Adrian on 2/21/2014.
+//  Copyright (c) 2014 Creatif Cubed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
